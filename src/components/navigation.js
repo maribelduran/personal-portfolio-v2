c44b0cfd9dd63a94c283c00c657d860b26e7cbba
Maribel Duran <maribelduranucm@gmail.com>
2018-04-05 17:36:41 -0700

    Add styling to navigation component

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,6 +1,41 @@
 import React from "react";
+import { Navbar, Nav, NavItem} from 'react-bootstrap';
+import  "./navigation.css";
+
+const { 
+  Header, 
+  Brand,
+  Toggle, 
+  Collapse 
+  } = Navbar;
 
 const Navigation = () => 
+<Navbar className="no-botton-border" fixedTop={true}>
+<Header>
+    <Brand>
+      <a href="#top">MARIBEL DURAN</a>
+    </Brand>
+    <Toggle />
+  </Header>
+  <Collapse>
+  <Nav pullRight>
+    <NavItem eventKey={1} href="#about">
+      ABOUT ME
+    </NavItem>
+    <NavItem eventKey={2} href="#projects">
+      PROJECTS
+    </NavItem>
+    <NavItem eventKey={3} href="#contact">
+      CONTACT
+    </NavItem>
+    <NavItem eventKey={3} href="https://medium.com/@maribelduran">
+      WRITING
+    </NavItem>
+  </Nav>
+  </Collapse>
+</Navbar>
+
+/*
 <nav className="navbar navbar-fixed-top">
   <div className="navbar-header">
     <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
@@ -16,10 +51,11 @@ const Navigation = () =>
     <ul className="nav navbar-nav navbar-right">
       <li><a href="#about">ABOUT ME</a></li>
       <li><a href="#projects">PROJECTS</a></li>
-      <li><a href="#contact">CONTACT</a></li>
+      <li><a href=".contact">CONTACT</a></li>
       <li><a href="https://medium.com/@maribelduran" target="_blank" >WRITING</a></li>
     </ul>
   </div>
   </nav>
+*/
 
 export default Navigation;
