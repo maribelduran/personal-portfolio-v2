2d172a45be476109304c6804cda4ffaa67cb4c30
Maribel Duran <maribelduranucm@gmail.com>
2018-04-06 13:19:28 -0700

    Add styling

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,11 @@
 import React from "react";
+import "./footer.css";
 
 const Footer = () =>
 <footer>
-<div>
-    <p>Inspired by many <i class="fa fa-heart" aria-hidden="true"></i></p>
+
+    <p>Inspired by many <i className="fa fa-heart" aria-hidden="true"></i></p>
     <p>Coded by Maribel Duran</p>
-</div>
 </footer>
 
 export default Footer;
