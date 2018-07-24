98676239670e06a0c55ed2d9a47717713f475d6b
Maribel Duran <maribelduranucm@gmail.com>
2018-07-23 18:28:12 -0700

    Replace cover background image with Gatsby Img Component

--- src/components/cover.js
+++ src/components/cover.js
@@ -1,9 +1,22 @@
 import React from "react";
 import Scrollchor from "react-scrollchor";
+import Img from 'gatsby-image'
 import  "./cover.css";
 
-const Cover = () =>
-<div id="#top" className="cover animated fadeIn">
+const Cover = ({coverImg}) =>
+<div id="#top" className="cover animated ">
+  <Img
+      title="Cover image"
+      alt="Air Balloon in Nature"
+      sizes={coverImg.sizes}
+      style={{
+        position: "absolute",
+        left: 0,
+        top: 0,
+        width: "100%",
+        height: "100%"
+      }}
+    />
   <div className="overlay"></div>
   <div className="center">
     <h1 className="name">
