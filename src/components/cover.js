31236e03fedc59c656d273e5acf85858542fbe74
Maribel Duran <maribelduranucm@gmail.com>
2018-07-27 16:56:33 -0700

    Fix indentation using prettier

--- src/components/cover.js
+++ src/components/cover.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Scrollchor from "react-scrollchor";
-import Img from 'gatsby-image'
-import  "./cover.css";
+import Img from "gatsby-image";
+import "./cover.css";
 
-const Cover = ({coverImg}) =>
-<div id="#top" className="cover animated ">
-  <Img
+const Cover = ({ coverImg }) => (
+  <div id="#top" className="cover animated ">
+    <Img
       title="Cover image"
       alt="Air Balloon in Nature"
       sizes={coverImg.sizes}
@@ -17,18 +17,23 @@ const Cover = ({coverImg}) =>
         height: "100%"
       }}
     />
-  <div className="overlay"></div>
-  <div className="center">
-    <h1 className="name">
-      <b>HI, I'M MARIBEL</b>
-    </h1>
-    <p className="greetings">Let's change the world together!</p>
+    <div className="overlay" />
+    <div className="center">
+      <h1 className="name">
+        <b>HI, I'M MARIBEL</b>
+      </h1>
+      <p className="greetings">Let's change the world together!</p>
+    </div>
+    <div className="arrow animated bounceInDown">
+      <Scrollchor
+        to="#about"
+        className="btn btn-md"
+        animate={{ duration: 1000 }}
+      >
+        <span className="glyphicon glyphicon-menu-down" />
+      </Scrollchor>
+    </div>
   </div>
-  <div className="arrow animated bounceInDown">
-  <Scrollchor to="#about" className="btn btn-md" animate={{ duration: 1000}}>
-      <span className="glyphicon glyphicon-menu-down"></span>
-  </Scrollchor>
-  </div>
-</div>
+);
 
 export default Cover;
