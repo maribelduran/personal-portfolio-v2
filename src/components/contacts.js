6897106befaebf3883770dcbe8e808f1c8b0bf72
Maribel Duran <maribelduranucm@gmail.com>
2018-07-27 16:48:07 -0700

    Add unique key for each contact in contactList

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -3,12 +3,12 @@ import "./contacts.css";
 import contactList from "../data/contacts.json"
 
 const Contacts = () => {
-return(
+  return(
   <section id="contact" className="section contact">
     <h2 className="text-center">CONTACT</h2>
     <div className="row text-center section-content">
       {contactList.map(contact => 
-        <div className="col-sm-2">
+        <div className="col-sm-2" key={contact.url}>
           <a href={contact.url} target="_blank" className="btn btn-social-icon">
             <span className={`fa fa-${contact.icon}`}></span>
           </a>
