5f011f076f22b496a687c4a489d543ea600d9409
Maribel Duran <maribelduranucm@gmail.com>
2018-07-28 12:53:54 -0700

    Remove btn class from contact icons

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -1,22 +1,22 @@
 import React from "react";
 import "./contacts.css";
-import contactList from "../data/contacts.json"
+import contactList from "../data/contacts.json";
 
 const Contacts = () => {
-  return(
-  <section id="contact" className="section contact">
-    <h2 className="text-center">CONTACT</h2>
-    <div className="row text-center section-content">
-      {contactList.map(contact => 
-        <div className="col-sm-2" key={contact.url}>
-          <a href={contact.url} target="_blank" className="btn btn-social-icon">
-            <span className={`fa fa-${contact.icon}`}></span>
-          </a>
-        </div>
-      )}
-    </div>
+  return (
+    <section id="contact" className="section contact">
+      <h2 className="text-center">CONTACT</h2>
+      <div className="row text-center section-content">
+        {contactList.map(contact => (
+          <div className="col-sm-2" key={contact.url}>
+            <a href={contact.url} target="_blank" className="btn-social-icon">
+              <span className={`fa fa-${contact.icon}`} />
+            </a>
+          </div>
+        ))}
+      </div>
     </section>
-  )
-}
+  );
+};
 
 export default Contacts;
