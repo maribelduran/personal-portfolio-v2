9bd63d0e05182329fcc62a8ce77cd690c4a30f2b
maribelduran <maribelduranucm@gmail.com>
2019-12-22 18:03:57 -0800

    Transform class component to function component

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,112 +1,88 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Collapse,
   Navbar,
   NavbarToggler,
   NavbarBrand,
   NavLink,
-  Nav,
-  NavItem
+  NavItem,
 } from "reactstrap";
 import ScrollchorItem from "./scrollchor-item";
 import Scrollspy from "react-scrollspy";
 import "./navigation.css";
 
-class Navigation extends Component {
-  constructor(props) {
-    super(props);
+const Navigation = () => {
+  const [scrolledDownEnough, setScrolledDownEnough] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    this.state = {
-      hasScrolledDown: false,
-      isOpen: false
-    };
-    this.handleScroll = this.handleScroll.bind(this);
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.handleCloseCollapse = this.handleCloseCollapse.bind(this);
-  }
-
-  handleScroll() {
-    const bodyScrollTop =
-      document.documentElement.scrollTop || document.body.scrollTop;
-    let scrolledDownEnough = bodyScrollTop > 75 ? true : false;
-    this.setState({
-      hasScrolledDown: scrolledDownEnough
-    });
-  }
-
-  toggleNavbar() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
-
-  handleCloseCollapse() {
-    if (this.state.isOpen) {
-      this.setState({
-        isOpen: !this.state.isOpen
-      });
+  const handleCloseCollapse = () => {
+    if (isOpen) {
+      setIsOpen(!isOpen);
     }
-  }
+  };
 
-  componentDidMount() {
-    window.addEventListener("scroll", this.handleScroll);
-  }
+  useEffect(() => {
+    const handleScroll = () => {
+      const bodyScrollTop =
+        document.documentElement.scrollTop || document.body.scrollTop;
+      const scrolledDownEnough = bodyScrollTop > 75 ? true : false;
+      setScrolledDownEnough(scrolledDownEnough);
+    };
 
-  componentWillUnmount() {
-    window.removeEventListener("scroll", this.handleScroll);
-  }
+    window.addEventListener("scroll", handleScroll, { passive: true });
 
-  render() {
-    const whiteBackground = this.state.hasScrolledDown
-      ? "white-background navbar-border"
-      : "";
-    const fontColor = this.state.hasScrolledDown ? "blue-font" : "white-font";
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [scrolledDownEnough]);
 
-    return (
-      <Navbar className={whiteBackground} fixed={"top"} expand="md">
-        <NavbarBrand href="https://www.maribelduran.com/" className={fontColor}>
-          MARIBEL DURAN
-        </NavbarBrand>
-        <NavbarToggler onClick={this.toggleNavbar}>
-          <i className={`fa fa-navicon ${fontColor}`} />
-        </NavbarToggler>
-        <Collapse isOpen={this.state.isOpen} className={`${fontColor}`} navbar>
-          <Scrollspy
-            items={["about", "projects", "contact"]}
-            currentClassName="active"
-            className={`${fontColor} ml-auto navbar-nav`}
-            navbar
-          >
-            <NavItem onClick={this.handleCloseCollapse}>
-              <ScrollchorItem to="#about" className="nav-link">
-                ABOUT ME
-              </ScrollchorItem>
-            </NavItem>
-            <NavItem onClick={this.handleCloseCollapse}>
-              <ScrollchorItem to="#projects" className="nav-link">
-                PROJECTS
-              </ScrollchorItem>
-            </NavItem>
-            <NavItem onClick={this.handleCloseCollapse}>
-              <ScrollchorItem to="#contact" className="nav-link">
-                CONTACT
-              </ScrollchorItem>
-            </NavItem>
-            <NavItem onClick={this.handleCloseCollapse}>
-              <NavLink
-                href="https://medium.com/@maribelduran"
-                target="_blank"
-                className="external-link"
-                rel="noopener noreferrer"
-              >
-                WRITING
-              </NavLink>
-            </NavItem>
-          </Scrollspy>
-        </Collapse>
-      </Navbar>
-    );
-  }
-}
+  const whiteBackground = scrolledDownEnough
+    ? "white-background navbar-border"
+    : "";
+  const fontColor = scrolledDownEnough ? "blue-font" : "white-font";
+
+  return (
+    <Navbar className={whiteBackground} fixed={"top"} expand="md">
+      <NavbarBrand href="https://www.maribelduran.com/" className={fontColor}>
+        MARIBEL DURAN
+      </NavbarBrand>
+      <NavbarToggler onClick={() => setIsOpen(!isOpen)}>
+        <i className={`fa fa-navicon ${fontColor}`} />
+      </NavbarToggler>
+      <Collapse isOpen={isOpen} className={`${fontColor}`} navbar>
+        <Scrollspy
+          items={["about", "projects", "contact"]}
+          currentClassName="active"
+          className={`${fontColor} ml-auto navbar-nav`}
+          navbar
+        >
+          <NavItem onClick={handleCloseCollapse}>
+            <ScrollchorItem to="#about" className="nav-link">
+              ABOUT ME
+            </ScrollchorItem>
+          </NavItem>
+          <NavItem onClick={handleCloseCollapse}>
+            <ScrollchorItem to="#projects" className="nav-link">
+              PROJECTS
+            </ScrollchorItem>
+          </NavItem>
+          <NavItem onClick={handleCloseCollapse}>
+            <ScrollchorItem to="#contact" className="nav-link">
+              CONTACT
+            </ScrollchorItem>
+          </NavItem>
+          <NavItem onClick={handleCloseCollapse}>
+            <NavLink
+              href="https://medium.com/@maribelduran"
+              target="_blank"
+              className="external-link"
+              rel="noopener noreferrer"
+            >
+              WRITING
+            </NavLink>
+          </NavItem>
+        </Scrollspy>
+      </Collapse>
+    </Navbar>
+  );
+};
 
 export default Navigation;
